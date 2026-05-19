b776b379b1090a376251407c6a32f2fd10edacee
RafaelCaccini <rafaeldesouzalins765@gmail.com>
2026-05-18 22:17:20 -0300

    FUNCIONOU MANO O MENU GG

--- Projeto-Abismo/Assets/Scripts/Menu/MenuPrincipalManager.cs
+++ Projeto-Abismo/Assets/Scripts/Menu/MenuPrincipalManager.cs
@@ -1,32 +1,123 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuPrincipalManager : MonoBehaviour
 {
+    [Header("CENA PRINCIPAL")]
     [SerializeField] private string nomeDaCena;
+
+    [Header("FASES")]
+    [SerializeField] private string fase1 = "Parte1";
+
+    [SerializeField] private string fase2 = "Parte2";
+
+    [SerializeField] private string fase3 = "Parte3";
+
+    [SerializeField] private string fase4 = "Parte4";
+
+    [SerializeField] private string fase5 = "Parte5";           
+
+
+    [Header("PAINÉIS")]
     [SerializeField] private GameObject painelMenuInicial;
+
     [SerializeField] private GameObject painelOpcoes;
+
+    [SerializeField] private GameObject painelFases;
+
+    // =====================================
+    // JOGAR
+    // =====================================
 
     public void Jogar()
     {
         SceneManager.LoadScene(nomeDaCena);
     }
 
+    // =====================================
+    // FASES
+    // =====================================
+
+    public void AbrirFases()
+    {
+        painelMenuInicial.SetActive(false);
+
+        painelFases.SetActive(true);
+    }
+
+    public void FecharFases()
+    {
+        painelFases.SetActive(false);
+
+        painelMenuInicial.SetActive(true);
+    }
+
+    // =====================================
+    // CARREGAR FASES
+    // =====================================
+
+    public void IrParaParte1()
+    {
+        Debug.Log("Indo para Parte1");
+
+        SceneManager.LoadScene(fase1);
+    }
+
+    public void IrParaParte2()
+    {
+        Debug.Log("Indo para Parte2");
+
+        SceneManager.LoadScene(fase2);
+    }
+
+    public void IrParaParte3()
+    {
+        Debug.Log("Indo para Parte3");
+
+        SceneManager.LoadScene(fase3);
+    }
+
+    public void IrParaParte4()
+    {
+        Debug.Log("Indo para Parte4");
+
+        SceneManager.LoadScene(fase4);
+    }
+
+    public void IrParaParte5()
+    {
+        Debug.Log("Indo para Parte5");
+        SceneManager.LoadScene(fase5);
+    }
+
+    // =====================================
+    // OPÇÕES
+    // =====================================
+
     public void AbrirOpcoes()
     {
         painelMenuInicial.SetActive(false);
+
         painelOpcoes.SetActive(true);
     }
 
     public void FecharOpcoes()
     {
         painelOpcoes.SetActive(false);
+
         painelMenuInicial.SetActive(true);
     }
 
+    // =====================================
+    // SAIR
+    // =====================================
+
     public void SairJogo()
     {
-        Debug.Log("Saindo do jogo...(ELE S� FUNCIONAA COM O JOGO BUILDAADO!!!!!!!)");
+        Debug.Log(
+            "Saindo do jogo... (só funciona buildado)"
+        );
+
         Application.Quit();
     }
 }
---
+++ Projeto-Abismo/Assets/Scripts/Mundo/Backup Player/PlayerSceneLoader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerSceneLoader : MonoBehaviour
+{
+    [Header("REFERÊNCIAS")]
+    [SerializeField] private Transform spawnPoint;
+
+    [SerializeField] private GameObject backupPlayer;
+
+    private void Start()
+    {
+        GameObject player =
+            GameObject.FindGameObjectWithTag(
+                "Player"
+            );
+
+        // =========================
+        // NÃO EXISTE PLAYER
+        // =========================
+
+        if (player == null)
+        {
+            Debug.Log(
+                "Player não encontrado"
+            );
+
+            if (backupPlayer != null)
+            {
+                backupPlayer.SetActive(true);
+
+                player = backupPlayer;
+
+                Debug.Log(
+                    "Backup ativado"
+                );
+            }
+            else
+            {
+                Debug.LogError(
+                    "BackupPlayer NULL"
+                );
+
+                return;
+            }
+        }
+
+        // =========================
+        // TELEPORTAR
+        // =========================
+
+        player.transform.position =
+            spawnPoint.position;
+
+        Debug.Log(
+            "Player movido pro spawn"
+        );
+    }
+}
